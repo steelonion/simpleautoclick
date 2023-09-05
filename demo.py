1568878fc92c4202d551e6942da67a44fd2d488c
SteelOnion <steelonion@foxmail.com>
2023-09-05 22:29:39 +0800

    添加键盘记录和鼠标回放

--- demo.py
+++ demo.py
@@ -1,10 +1,12 @@
 import simple_record
 import time
 
-recorder= simple_record.MouseRecorder('q')
+recorder= simple_record.InputRecorder('q')
 recorder.start()
 time.sleep(5)
 recorder.stop()
 save= simple_record.RecorderSave()
 recorder.export(save)
 save.write("233")
+replay=simple_record.Replayer(save)
+replay.replay()
--- simple_record.py
+++ simple_record.py
@@ -2,6 +2,22 @@ import json
 from pynput import keyboard, mouse
 import time
 from enum import IntEnum
+import pyautogui
+
+
+class Replayer:
+    def __init__(self, save):
+        self.save = save
+        self.start_timestamp = 0
+
+    def replay(self):
+        print("replay now...")
+        self.start_timestamp = time.time()
+        for action in self.save.content["mouse_move"]:
+            while (action[0] > time.time()-self.start_timestamp):
+                time.sleep(0.1)
+            if (action[1] == EAction.mousemove.value):
+                pyautogui.moveTo(action[2], action[3])
 
 
 class EAction(IntEnum):
@@ -10,14 +26,15 @@ class EAction(IntEnum):
     keyup = 4
 
 
-class MouseRecorder:
-    # 鼠标记录器
+class InputRecorder:
+    # 记录器
     def __init__(self, quit_key: str = 'q'):
-        self.record_interval=0.1
-        self.next_record_time=0
+        self.record_interval = 0.2
+        self.next_record_time = 0
         self.running = False
         self.quit_key = quit_key
         self.position_list = []
+        self.key_list = []
         self.start_timestamp = 0
         self.keyboard_listener = keyboard.Listener(
             on_press=self.on_key_press, on_release=self.on_key_release)
@@ -27,7 +44,7 @@ class MouseRecorder:
 
     def start(self):
         self.running = True
-        self.next_record_time=0
+        self.next_record_time = 0
         self.start_timestamp = time.time()
         self.keyboard_listener.start()
         self.mouse_listener.start()
@@ -40,22 +57,25 @@ class MouseRecorder:
     def export(self, save):
         # 导出当前数据
         save.content["mouse_move"] = self.position_list
+        save.content["key_action"] = self.key_list
 
     # 键盘事件回调函数
     def on_key_press(self, key):
-        print(key)
         if (key.char == self.quit_key):
             self.stop()
-        print(f'键盘按下: {key}')
+            return
+        timespan = time.time()-self.start_timestamp
+        self.key_list.append((timespan, EAction.keydown, key.char))
 
     def on_key_release(self, key):
-        print(f'键盘释放: {key}')
+        timespan = time.time()-self.start_timestamp
+        self.key_list.append((timespan, EAction.keyup, key.char))
 
     # 鼠标事件回调函数
     def on_mouse_move(self, x, y):
         timespan = time.time()-self.start_timestamp
-        if(timespan>self.next_record_time):
-            self.next_record_time=timespan+self.record_interval
+        if (timespan > self.next_record_time):
+            self.next_record_time = timespan+self.record_interval
             self.position_list.append((timespan, EAction.mousemove, x, y))
             print(f'鼠标移动到 ({x}, {y})')
 
